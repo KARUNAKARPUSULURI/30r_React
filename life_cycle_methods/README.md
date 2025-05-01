219f63fc92a843c35825eaf537767528ec1cdb6e
KARUNAKARPUSULURI <karunpusuluri@gmail.com>
2025-05-01 10:23:03 +0530

    mounting phase

---
+++ life_cycle_methods/README.md
@@ -0,0 +1,20 @@
+ methodsLife cycle : -
+Here we are going to discuss about component's life cycle 
+
+Creation Phase or Mounting Phase:- This phase is all about creating a component and inserting into the DOM...
+
+1. constructor(){} -> this gets called when class is being initialized
+2. render(){} -> it is used to return the jsx
+3. ComponentDidMount(){} ->This gets called after render means browser finishes painting UI, and this method will get called once. 
+-> To perform side effects 
+1. Function calls
+2. Fetch api 
+3. Timer functions
+4. Subscriptions
+
+
+
+
+Updation Phase :- This gets trigger whenever a particular value gets updated
+
+Deletetion Phase or unmounting phase : - Component gets deleted or removed
---
+++ life_cycle_methods/src/App.jsx
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      count: 0,
+      data: []
+    }
+    console.log("Constructor")
+  }
+
+  fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products")
+    const data = response.json()
+    this.setState({data : data})
+  }
+
+
+  componentDidMount() {
+    console.log("component did mount")
+    // this.fetchProducts()
+    fetch("https://fakestoreapi.com/products").then(res => res.json()).then(data => this.setState({data : data}))
+  }
+
+  render() {
+    console.log("render", this.state.data)
+    // console.log("fetching...",this.fetchProducts())
+    return (
+      <> 
+        <h1>Counter : {this.state.count}</h1>
+        <button onClick={() => this.setState({ count: this.state.count + 1 })}>Click</button>
+        {
+          this.state.data.length > 0 && this.state.data.map((product, index)=>{
+            return <p>{product.title}</p>
+          })
+        }
+      </>
+    )
+  }
+
+}
+
+export default App
